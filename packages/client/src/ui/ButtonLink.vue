b24dbc69263d7a44f83ccd7e67fe061236ac85f0
velhrh <valentyn.yurchenko@sysgears.com>
2026-08-02 21:17:31 +0300

    feat(client): redesign button system (#61)

--- packages/client/src/ui/ButtonLink.vue
+++ packages/client/src/ui/ButtonLink.vue
@@ -4,17 +4,25 @@ import {
   type RouteLocationAsPathGeneric,
   type RouteLocationAsRelativeGeneric,
 } from "vue-router";
+import { buttonBaseClasses, sizeClasses, variantClasses } from "./button/button-config.ts";
+import { ButtonSize } from "./button/button-size";
 import { ButtonVariant } from "./button/button-variant";
-import type { ButtonProps } from "./button/Button.vue";
-import { variantClasses } from "./button/button-config.ts";
 
-const { to } = defineProps<
-  ButtonProps & { to: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric }
->();
+type ButtonLinkProps = {
+  size?: ButtonSize;
+  to: string | RouteLocationAsRelativeGeneric | RouteLocationAsPathGeneric;
+  variant?: ButtonVariant;
+};
+
+const {
+  size = ButtonSize.DEFAULT,
+  to,
+  variant = ButtonVariant.LINK,
+} = defineProps<ButtonLinkProps>();
 </script>
 
 <template>
-  <RouterLink :to="to" :class="variantClasses[ButtonVariant.LINK]">
+  <RouterLink :to="to" :class="[buttonBaseClasses, sizeClasses[size], variantClasses[variant]]">
     <slot />
   </RouterLink>
 </template>
---
+++ packages/client/src/ui/button/button-size.ts
@@ -0,0 +1,4 @@
+export enum ButtonSize {
+  DEFAULT = "DEFAULT",
+  COMPACT = "COMPACT",
+}
